5909a0375e8339672b8533b22b14d4131168247d
Alex Miller <alexiscool2003@sbcglobal.net>
2023-09-27 00:06:09 -0400

    added system theme and theme saving

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RootStack from "./navigator/RootStack";
 import { ThemeContext } from "./contexts/ThemeContext";
+import { Appearance } from "react-native";
+import { storeData, getData } from "./config/asyncStorage";
+import * as SplashScreen from "expo-splash-screen";
+
+// keep the splash screen visible while we fetch the resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [theme, setTheme] = useState({ mode: "dark" });
@@ -9,10 +15,45 @@ export default function App() {
     let mode;
     if (!newTheme) {
       mode = theme.mode === "dark" ? "light" : "dark";
-      newTheme = { mode };
+      newTheme = { mode, system: false };
+    } else {
+      if (newTheme.system) {
+        const systemColorScheme = Appearance.getColorScheme();
+        mode = systemColorScheme === "dark" ? "dark" : "light";
+
+        newTheme = { ...newTheme, mode };
+      } else {
+        newTheme = { ...newTheme, system: false };
+      }
     }
     setTheme(newTheme);
+    storeData("theme", newTheme);
   };
+
+  // moniter system for theme change
+  if (theme.system) {
+    Appearance.addChangeListener(({ colorScheme }) => {
+      updateTheme({ system: true, mode: colorScheme });
+    });
+  }
+
+  const fetchStoredTheme = async () => {
+    try {
+      const themeData = await getData("theme");
+
+      if (themeData) {
+        updateTheme(themeData);
+      }
+    } catch ({ message }) {
+      alert(message);
+    } finally {
+      await setTimeout(() => SplashScreen.hideAsync(), 1000);
+    }
+  };
+
+  useEffect(() => {
+    fetchStoredTheme();
+  }, []);
 
   return (
     <ThemeContext.Provider value={{ theme, updateTheme }}>
