1bcf7bc510d5201c4effd42a88b8511d73c116d4
Jaden Brookens <jlb224@calvin.edu>
2023-10-18 17:36:14 -0400

    more editing

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,52 +1,3 @@
-// import { StatusBar } from "expo-status-bar";
-// import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity} from "react-native";
-// import { Colors } from "../components/styles";
-// import { ThemeContext } from "../contexts/ThemeContext";
-// import {React, useContext} from "react";
-// import PropTypes from "prop-types";
-
-// const Profile = ({ navigation }) => {
-//   const {theme} = useContext(ThemeContext);
-//   let activeColors = Colors[theme.mode]
-
-//   return (
-//     <SafeAreaView
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//         backgroundColor: activeColors.background,
-//       }}
-//     >
-//       <TouchableOpacity onPress={() => navigation.navigate("Homepage")}>
-//         <Text
-//           style={{
-//             color: activeColors.primary,
-//           }}
-//           >
-//           Profile
-//         </Text>
-//       </TouchableOpacity>
-//     </SafeAreaView>
-//   );
-// };
-
-// Profile.propTypes = {
-//   navigation: PropTypes.shape({
-//     navigate: PropTypes.func.isRequired,
-//   }).isRequired,
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     //backgroundColor: Colors.background,
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
-
-// export default Profile;
 import React, { useContext } from "react";
 import {
   StyleSheet,
@@ -233,7 +184,7 @@ const styles = StyleSheet.create({
   infoContainer: {
     alignSelf: "center",
     alignItems: "center",
-    marginTop: 16,
+    marginTop: -60,
   },
   statsContainer: {
     flexDirection: "row",
