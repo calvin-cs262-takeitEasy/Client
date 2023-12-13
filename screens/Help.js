3b151db53a8afbe4fb5e069b159ae8ab57b386a7
palmerford <palmerjford@gmail.com>
2023-12-12 20:13:02 -0500

    added margins and headers

--- screens/Help.js
+++ screens/Help.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, Dimensions } from "react-native";
 import PropTypes from "prop-types";
 import { Colors } from "../components/styles";
 import { ThemeContext } from "../contexts/ThemeContext";
@@ -21,24 +21,113 @@ const Forgot = ({ navigation }) => {
       
     >
         <Header navigation={navigation} name="Help" type="backButton" />
-        
+        <View
+            style={{
+              backgroundColor: activeColors.background,
+              width: Dimensions.get("window").width - 20,
+              alignItems: "left",
+              paddingHorizontal: 15,
+              paddingTop: 10,
+            }}
+          >
+            <Text style={{ color: activeColors.text, fontSize: 25, marginBottom: 5 }}>Alarm:</Text>
+            <View
+              style={{
+                borderBottomColor: activeColors.tertiary,
+                borderBottomWidth: 3,
+                width: '40%',
+                borderRadius: 2,
+              }}
+            />
+            {/* Adjusted paddingBottom to give space between text and line */}
+            <View
+              style={{
+                paddingBottom: 0, 
+              }}
+            />
+          </View>
+
         <Text
           style={{
             color: activeColors.text,
+            paddingHorizontal: 25,
+            paddingTop: 10,
           }}
         >
-          Alarm:  {"\n"}
         Click on “Set Alarm” and set an alarm with the 24 hour clock.{"\n"}
-        If you dont turn the alarm off within 30 seconds of it going off, we send a Comm to your friends! {"\n"}
-        {"\n"}
-        Study Timer: {"\n"}
+        If you dont turn the alarm off within 30 seconds of it going off, we send a Comm to your friends!{"\n"}
+        </Text>
+        <View
+            style={{
+              backgroundColor: activeColors.background,
+              width: Dimensions.get("window").width - 20,
+              alignItems: "left",
+              paddingHorizontal: 15,
+              paddingTop: 10,
+            }}
+          >
+            <Text style={{ color: activeColors.text, fontSize: 25, marginBottom: 5 }}>Study Timer:</Text>
+            <View
+              style={{
+                borderBottomColor: activeColors.tertiary,
+                borderBottomWidth: 3,
+                width: '40%',
+                borderRadius: 2,
+              }}
+            />
+            {/* Adjusted paddingBottom to give space between text and line */}
+            <View
+              style={{
+                paddingBottom: 0, 
+              }}
+            />
+          </View>
+
+        <Text
+          style={{
+            color: activeColors.text,
+            paddingHorizontal: 25,
+            paddingTop: 10,
+          }}
+        >        
         Click on the 00:00 to set Hours and Minutes for your timer, then click “Start Timer” when you are ready to start studying.{"\n"}
         If you leave the screen while the timer is going, we send a Comm to your friends!{"\n"}
-        {"\n"}
-        Bedtime Mode: {"\n"}
+        </Text>
+        <View
+            style={{
+              backgroundColor: activeColors.background,
+              width: Dimensions.get("window").width - 20,
+              alignItems: "left",
+              paddingHorizontal: 15,
+              paddingTop: 10,
+            }}
+          >
+            <Text style={{ color: activeColors.text, fontSize: 25, marginBottom: 5 }}>Bedtime Mode:</Text>
+            <View
+              style={{
+                borderBottomColor: activeColors.tertiary,
+                borderBottomWidth: 3,
+                width: '40%',
+                borderRadius: 2,
+              }}
+            />
+            {/* Adjusted paddingBottom to give space between text and line */}
+            <View
+              style={{
+                paddingBottom: 0, 
+              }}
+            />
+          </View>
+
+        <Text
+          style={{
+            color: activeColors.text,
+            paddingHorizontal: 25,
+            paddingTop: 10,
+          }}
+        >        
         Set a time for bedtime mode to start every night! {"\n"}
         You have one minute of grace time. If you go on your phone more than that while in bedtime mode, we send a Comm to your friends!{"\n"}
-
         </Text>
     </SafeAreaView>
   );
@@ -49,13 +138,5 @@ Forgot.propTypes = {
     navigate: PropTypes.func.isRequired,
   }).isRequired,
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default Forgot;
